403fcab6041333b2b1418dbbb5b3b6e384c6c2ff
odhis101 <joshodhiambo5@gmail.com>
2023-07-15 13:37:31 +0300

    updates

--- pages/Login.js
+++ pages/Login.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text,ImageBackground, View,Image } from 'react-native';
+import { StyleSheet, Text,ImageBackground, View,Image,TouchableOpacity } from 'react-native';
 import BackgroundImage from "../assets/background.png"
 import Logo from "../assets/logo.png"
+import InputFeild from "../components/inputFeilds/InputFeilds"
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 const Login= () => {
 
@@ -16,6 +19,29 @@ const Login= () => {
         source={Logo}
         style={styles.image}
       />
+ <Text style={styles.title}>Welcome to Ping</Text>
+<Text style={styles.subtitle}>Please sign in to continue</Text>
+
+      <InputFeild title="Username"/>
+      <InputFeild title="Password"/>
+      <View style={styles.signInContainer}>
+  <TouchableOpacity style={styles.signInButton}>
+    <LinearGradient
+      colors={['#41CFD6', '#5A54D2']}
+      start={{ x: 1, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.gradientBackground}
+    >
+      <Text style={styles.signInButtonText}>Sign In</Text>
+    </LinearGradient>
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.forgotPasswordButton}>
+    <Text style={styles.forgotPasswordText }>
+      Forgot Password?
+    </Text>
+  </TouchableOpacity>
+</View>
+
 </View>
                 
                 
@@ -42,14 +68,52 @@ const styles = StyleSheet.create({
     
    },
    credentials:{
-    marginLeft:'10%',
-    marginRight:'10%',
-    marginTop:'10%',
+    marginLeft:'5%',
+    marginRight:'5%',
+    marginTop:'30%',
     marginBottom:'10%',
-    borderColor: "purple",
-    borderWidth: 4,
 
-   }
+
+   },
+   title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginBottom:'10%',
+
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+    marginBottom:'2%',
+
+  },
+   signInButton: {
+    width: '30%',
+
+  },
+  gradientBackground: {
+    paddingVertical: 12,
+    borderRadius: 100,
+  },
+  signInContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  forgotPasswordText:{
+    fontSize: 16,
+    color: 'white',
+    marginLeft:'5%',
+  },
+  signInButtonText: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
    
   });
 export default Login;
