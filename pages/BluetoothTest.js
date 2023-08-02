633c834f3f6fb84e5f6cb610e3a4b5f369ba7e05
odhis101 <joshodhiambo5@gmail.com>
2023-08-02 14:54:41 +0300

    hello world

--- pages/BluetoothTest.js
+++ pages/BluetoothTest.js
@@ -1,31 +1,26 @@
-import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native";
-import BleManager from 'react-native-ble-manager';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Button, Text } from 'react-native';
+import BleManager from 'react-native-ble-plx';
+
 
 
 const BluetoothTest = () => {
-    useEffect(() => {
-        BleAdvertiser.setCompanyId(0x004C); // You can set your custom company ID here
-        BleAdvertiser.broadcast('MyApp', null, null, (error) => {
-          if (error) {
-            console.log('Advertising error:', error);
-          } else {
-            console.log('Advertising started.');
-          }
-        });
-    
-        return () => {
-          BleAdvertiser.stopBroadcast(() => {
-            console.log('Advertising stopped.');
-          });
-        };
-      }, []);
-    return (
-        <View style={{marginTop:200}}>
-      <Text>Advertising...</Text>
-        </View>
-    );
-}
+  const [discoveredDevices, setDiscoveredDevices] = useState([]);
+  const bleManager = new BleManager();
+  bleManager.startScanning();
+
+  return (
+    <View style={{marginTop:50}}>
+      <Text>Discovered Devices:</Text>
+      {discoveredDevices.length > 0 ? (
+        discoveredDevices.map(device => (
+          <Text key={device.id}>{device.name || 'Unknown Device'}</Text>
+        ))
+      ) : (
+        <Text>No devices available</Text>
+      )}
+    </View>
+  );
+};
 
 export default BluetoothTest;
-
